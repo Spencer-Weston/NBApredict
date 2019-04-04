b9ee6849658365dcc367d6435ac8bb8902e0314f
Spencer Weston <spencerweston3214@gmail.com>
2019-04-04 14:23:38 -0400

    Finished run/all documentation

--- run/all.py
+++ run/all.py
@@ -1,3 +1,12 @@
+"""Author: Spencer Weston
+
+This module runs the entire NBA_bet project.
+
+This module wraps the entire project into a single script with run_all() as the function which drives the script. First,
+it sets up the database and session connections. Then, it scrapes all new data. Finally, it predicts all games for which
+data is available. Most session.commit() calls in the project are performed here. However, note predict_all() requires
+a commit during the process in order to function correctly.
+"""
 from sqlalchemy.orm import Session
 
 # Local Imports
@@ -7,6 +16,7 @@ from scrapers import scraper
 
 
 def run_all():
+    """Run the entire NBA_bet project."""
     db = Database()
     year = 2019
     session = Session(bind=db.engine)
