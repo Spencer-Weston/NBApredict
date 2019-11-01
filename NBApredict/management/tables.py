67eb7b1fb1a63929ce111634b0d597c1ca843f29
Spencer Weston <spencerweston3214@gmail.com>
2019-11-01 12:36:55 -0400

    Schedule table setup complete; does not yet perform updates

--- NBApredict/management/tables.py
+++ NBApredict/management/tables.py
@@ -17,12 +17,12 @@ def create_team_table(db, teams_data, tbl_name):
     """
     columns = teams_data.columns
     columns["team_name"].append({"unique": True})
-    db.map_table(tbl_name=tbl_name, column_types=columns)
+    db.map_table(tbl_name=tbl_name, columns=columns)
     db.create_tables()
     db.clear_mappers()
 
 
-def create_team_stats_table(db, team_stats_data, team_tbl, tbl_name):
+def create_team_stats_table(db, team_stats_data, tbl_name):
     """Create a table of team stats in a database with appropriate foreign keys and constraints.
 
     Args:
@@ -34,9 +34,9 @@ def create_team_stats_table(db, team_stats_data, team_tbl, tbl_name):
     ToDo: Currently allows duplicate rows if those values are on different days. Solve with a constraint
     """
     columns = team_stats_data.columns
-    columns['team_id'].append(ForeignKey("{}.id".format(team_tbl.__table__.fullname)))
-    constraints = {UniqueConstraint: ["team_id", "scrape_time"]}
-    db.map_table(tbl_name=tbl_name, column_types=columns, constraints=constraints)
+    columns['team_id'].append(ForeignKey("teams_{}.id".format(Config.get_property('league_year'))))
+    constraints = [UniqueConstraint("team_id", "scrape_time")]
+    db.map_table(tbl_name=tbl_name, columns=columns, constraints=constraints)
     db.create_tables()
     db.clear_mappers()
 
@@ -69,8 +69,12 @@ def format_schedule_data(schedule_data, team_tbl, team_stats_tbl):
         team_tbl: A mapped instance of the team_tbl
         team_stats_tbl: A mapped instance of the team_stats_tbl
     """
-    schedule_data.data['MOV'] = [0]  # Adds a 'MOV' an integer column
-    schedule_data.fill('MOV', None)
+    h_score = schedule_data.data['home_team_score']
+    a_score = schedule_data.data['away_team_score']
+    schedule_data.data['MOV'] = [h_score[i]-a_score[i] for i in range(schedule_data.num_rows())]
+    schedule_data.data['playoffs'] = ['']
+    schedule_data.fill('playoffs', None)
+
     schedule_data.data["home_team_id"] = values_to_foreign_key(team_tbl, "id", "team_name",
                                                                schedule_data.data.pop("home_team"))
     schedule_data.data["away_team_id"] = values_to_foreign_key(team_tbl, "id", "team_name",
@@ -78,7 +82,7 @@ def format_schedule_data(schedule_data, team_tbl, team_stats_tbl):
 
     today = datetime.date(datetime.now())
     subquery = session.query(team_stats_tbl.id, team_stats_tbl.team_id, func.max(team_stats_tbl.scrape_time)). \
-        filter(team_stats_tbl.scrape_time < today).group_by(team_stats_tbl.team_id).subquery()
+        filter(team_stats_tbl.scrape_date <= today).group_by(team_stats_tbl.team_id).subquery()
     schedule_data.data['home_stats_id'] = values_to_foreign_key(subquery, 'id', 'team_id',
                                                                 schedule_data.data['home_team_id'])
     schedule_data.data['away_stats_id'] = values_to_foreign_key(subquery, 'id', 'team_id',
@@ -88,7 +92,14 @@ def format_schedule_data(schedule_data, team_tbl, team_stats_tbl):
 
 
 def create_schedule_table(db, schedule_data, tbl_name, team_tbl, team_stats_tbl):
-
+    """Create a table of the NBA schedule in the database.
+    Args:
+        db: a datotable.database.Database object connected to a database
+        schedule_data: A datatotable.data.DataOperator object with schedule data
+        tbl_name: The desired name of the table
+        team_tbl: A mapped team table to set foreign keys on
+        team_stats_tbl: A mapped team stats table to set foreign keys on
+    """
     columns = schedule_data.columns
     team_tbl_name = team_tbl.__table__.fullname
     team_stats_tbl_name = team_stats_tbl.__table__.fullname
@@ -155,6 +166,7 @@ def main(db, session):
     team_dict = team_scraper.scrape()
     teams_data = DataOperator({"team_name": team_dict["team_name"]})
 
+    # Teams
     teams_tbl_name = "teams_{}".format(year)
     if not db.table_exists(teams_tbl_name):
         create_team_table(db=db, teams_data=teams_data, tbl_name=teams_tbl_name)
@@ -163,15 +175,21 @@ def main(db, session):
         session.commit()
         del teams_tbl
 
+    # Team Stats
     team_stats_tbl_name = "team_stats_{}".format(year)
     teams_tbl = db.table_mappings[teams_tbl_name]
     team_dict['team_id'] = team_dict.pop('team_name')
     team_dict['team_id'] = values_to_foreign_key(foreign_tbl=teams_tbl, foreign_key="id", foreign_value="team_name",
                                                  child_data=team_dict['team_id'])
+    # When team_stats_tbl is created, the teams_tbl automap object is changed. The changed format does not follow
+    # the expected behavior of an automapped table. I suspect this is because a relationship is established.
+    # If we reloaded, teams_tbl works fine. Therefore, delete the variable here for now
+    del teams_tbl
+    team_dict['scrape_date'] = [datetime.date(s_time) for s_time in team_dict['scrape_time']]
     team_stats_data = DataOperator(team_dict)
     if not db.table_exists(team_stats_tbl_name):
-        create_team_stats_table(db=db, team_stats_data=team_stats_data, team_tbl=teams_tbl,
-                                tbl_name=team_stats_tbl_name)
+        create_team_stats_table(db=db, team_stats_data=team_stats_data, tbl_name=team_stats_tbl_name)
+
         team_stats_tbl = db.table_mappings[team_stats_tbl_name]
         session.add_all([team_stats_tbl(**row) for row in team_stats_data.rows])
         session.commit()
@@ -181,18 +199,23 @@ def main(db, session):
         team_stats_tbl = db.table_mappings[team_stats_tbl_name]
         update_team_stats_table(db, session, team_stats_tbl, team_stats_data)
 
-
+    # Schedule
     schedule_dict = season_scraper.scrape()
     schedule_data = DataOperator(schedule_dict)
+    teams_tbl = db.table_mappings['teams_{}'.format(year)]
     schedule_data = format_schedule_data(schedule_data, teams_tbl, team_stats_tbl)
     schedule_tbl_name = "schedule_{}".format(year)
     if not db.table_exists(schedule_tbl_name):
         create_schedule_table(db, schedule_data, schedule_tbl_name, teams_tbl, team_stats_tbl)
-        schedule_tbl = db.table_mappings[schedule_tbl_name]
+        # schedule_tbl = db.table_mappings[schedule_tbl_name]
     else:
         schedule_tbl = db.table_mappings[schedule_tbl_name]
         session.add_all([schedule_tbl(**row) for row in schedule_data.rows])
+        session.commit()
         # update_schedule_table()
+    schedule_tbl = db.table_mappings[schedule_tbl_name]
+    session.add_all([schedule_tbl(**row) for row in schedule_data.rows])
+    session.commit()
     t=2
 
 
