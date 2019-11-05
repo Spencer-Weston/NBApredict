47a374a594031486e84953e5dc6e85988f860615
Spencer Weston <spencerweston3214@gmail.com>
2019-11-05 11:48:40 -0500

    Linescraper now functions independently of the database

--- NBApredict/scrapers/line_scraper.py
+++ NBApredict/scrapers/line_scraper.py
@@ -3,6 +3,7 @@ line_scraper scrapes NBA betting odds from Bovada and stores them in the databas
 """
 
 from datetime import datetime, timedelta
+import re
 import requests
 from sqlalchemy import UniqueConstraint, ForeignKey, Integer
 from sqlalchemy.exc import IntegrityError
@@ -22,11 +23,10 @@ def bovada_json_request(url):
     return response
 
 
-def odds_for_today(games_query):
+def odds_for_today():
     """Match betting odds from Bovada to the games_query and return the odds
 
     Args:
-        games_query: A games query object typically returned from getters.get_games_on_day(); Should be the current
         date to reflect the current games on Bovada.
 
     Returns:
@@ -52,23 +52,26 @@ def odds_for_today(games_query):
         return None
 
     # Set-up the line dictionary which stores data in the correct table format
-    lines = {"home_team_id": [], "away_team_id": [], "spread": [], "home_spread_price": [],
+    lines = {"home_team_id": [], "away_team_id": [], 'start_time': [], "spread": [], "home_spread_price": [],
              "away_spread_price": [], "home_moneyline": [], "away_moneyline": [], "scrape_time": []}
 
     # Iterate through each game returned by bovada and store its information
     for game in bovada_games:
+        link = game['link']
+        str_time = re.findall('[0-9]', link)
+        start_time = ''.join(str_time)
+        start_time = datetime.strptime(start_time, "%Y%m%d%H%M")
+        if datetime.now() > start_time:
+            # An ongoing game will not have the correct betting data. We don't want to store this information
+            print("The game between {} and {} at {} is either ongoing or completed. Not scraping".format(
+                home_team, away_team, start_time))
+            continue
+
         home_team, away_team = parse_teams(game["competitors"])
 
         # Get only the full match betting information from the game object
         betting_info = game["displayGroups"][0]["markets"]
         full_match_bets = [bet for bet in betting_info if bet["period"]["description"] == "Match"]
-
-        # Find the game in games_query that matches the current game in bovada_games
-        game_tbl = [game for game in games_query if
-                    game.home_team == home_team.upper() and game.away_team == away_team.upper()]
-        if len(game_tbl) > 1:
-            raise Exception("Multiple games returned. Unexpected query result")
-        start_datetime = game_tbl[0].start_time
 
         # Extract the betting data associated with the game
         money_lines = False
@@ -81,17 +84,9 @@ def odds_for_today(games_query):
         if not money_lines:
             home_moneyline = None
             away_moneyline = None
-        try:
-            game_lines = [home_team, away_team, spread, home_spread_price, away_spread_price,
-                          home_moneyline, away_moneyline, scrape_time]
-        except NameError:
-            if datetime.now() > start_datetime:
-                # An ongoing game will not have the correct betting data. We don't want to store this information
-                print("The game between {} and {} at {} is either ongoing or completed. Not scraping".format(
-                    home_team, away_team, start_datetime))
-                continue
-            else:
-                raise NameError
+
+        game_lines = [home_team, away_team, start_time, spread, home_spread_price, away_spread_price, home_moneyline,
+                      away_moneyline, scrape_time]
 
         # This section depends on python 3.7+ to preserve the order of dict keys in lines
         i = 0
@@ -232,26 +227,10 @@ def update_odds_table(odds_table, sched_tbl, rows, session):
                 continue
 
 
-def scrape(database, session):
-    """Scrapes betting line information from bovada and adds it to the session
-
-    Args:
-        database: An instantiated DBInterface object from database.database for database interactions
-        session: An instance of a sqlalchemy Session class bound to the database's engine
-    """
+def scrape():
+    """Scrapes betting line information from bovada and adds it to the session"""
     league_year = Config.get_property("league_year")
-    schedule = database.table_mappings['schedule_{}'.format(league_year)]
-    date = datetime.date(datetime.now())
-    games = getters.get_games_on_day(schedule, session, date)
-
-    # If there are no games today, search for games up to 10 days past the current date
-    i = 1
-    while len(games.all()) < 1 and i <= 10:
-        date = date + timedelta(days=i)
-        games = getters.get_games_on_day(schedule, session, date)
-        i += 1
-
-    lines = odds_for_today(games)
+    lines = odds_for_today()
     if not lines:
         return False
     return lines
